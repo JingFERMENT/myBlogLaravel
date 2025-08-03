2c030b4fc7550f6bade3f08f98ffc7f6babe87aa
Jing ZHANG <jing.zhang.ferment@gmail.com>
2025-08-03 14:48:16 +0200

    feat: routing (name, parameters, request, groups)

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,37 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
+    return view('welcome');
+});
+
+// regrouper les routes du blog (pour changer facilement le préfixe)
+Route::prefix('/blog')->name('blog.')->group(function () {
+
+    Route::get('/', function (Request $request) {
+
+        return [
+            'link' => route('blog.show', [
+                'slug' => 'mon-super-article',
+                'id' => 1
+            ]),
+        ];
+    })->name('index'); // donner un nom à la route
+
+    // rajouter les paramètres dans l'URL
+    Route::get('/{slug}/{id}', function (string $slug, int $id, Request $request) {
+        return [
+            "slug" => $slug,
+            "id" => $id,
+        ];
+    })->where([
+        'slug' => '[a-zA-Z0-9\-]+',
+        'id' => '[0-9]+'
+    ])->name('show'); // ajouter les conditions sur les paramètres
+});
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
@@ -13,5 +39,5 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
 });
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
